6364199b43f5684c12e7f7a321c2d457e70ce558
Tamas Zolcsak <tamas_zolcsak@yahoo.com>
2018-02-10 12:30:14 +0100

    Initial application generated by JHipster-4.14.0

---
+++ src/main/java/org/spendings/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.spendings.service.dto;
---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+
+import {
+    SpendingsSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        SpendingsSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'en'
+        },
+    ],
+    exports: [
+        SpendingsSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class SpendingsSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
